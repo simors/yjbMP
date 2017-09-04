d8722ebea47e019d8758f36fade31c4fe1222a45
wanpeng <wanpeng19860919@163.com>
2017-09-04 18:25:49 +0800

    <Debug>

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,14 +7,40 @@ import { bindActionCreators } from 'redux'
 import { Button } from 'antd'
 import {requestPosition} from '../../actions/configActions'
 import {selectLocation} from '../../selector/configSelector'
+import * as appConfig from '../../constants/appConfig'
+import wx from 'tencent-wx-jssdk'
+
+
 
 class Home extends Component {
   constructor(props) {
     super(props)
   }
 
+  componentWillMount() {
+    wx.config({
+      "debug": true,
+      "appId": "wx792bf5a51051d512",
+      "timestamp": "1504520345",
+      "nonceStr": "a0eh123ysra10yn",
+      "signature": "82307404e403487f00704a3aa81a876ea1db5618",
+      "jsApiList": [
+        "scanQRCode"
+      ]
+    });
+    console.log("微信js-sdk wx:", wx)
+  }
+
   btnOnPress() {
     this.props.requestPosition({})
+  }
+
+  onScanQRCode = () => {
+    wx.scanQRCode({
+      needResult: 0,
+      scanType: ["qrCode","barCode"],
+      success: () => {}
+    })
   }
 
   render() {
@@ -26,6 +52,9 @@ class Home extends Component {
         </div>
         <div>
           {this.props.location ? this.props.location.address : ""}
+        </div>
+        <div>
+          <Button type="primary" onClick={this.onScanQRCode}>扫一扫</Button>
         </div>
       </div>
     )
