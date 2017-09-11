0de7ecbec8856051410e14ff28d50751e7f7efcd
wanpeng <wanpeng19860919@163.com>
2017-09-11 13:24:00 +0800

    <Debug>

--- src/components/Mine/Deposit.js
+++ src/components/Mine/Deposit.js
@@ -36,8 +36,9 @@ class Deposit extends Component {
   }
 
   createPaymentSuccessCallback = (charge) => {
+    var that = this
     pingpp.createPayment(charge, function (result, err) {
-      this.setState({disableButton: false})
+      that.setState({disableButton: false})
       if (result == "success") {
         // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL。
       } else if (result == "fail") {
--- src/components/Mine/Recharge.js
+++ src/components/Mine/Recharge.js
@@ -96,8 +96,9 @@ class Recharge extends Component {
   }
 
   createPaymentSuccessCallback = (charge) => {
+    var that = this
     pingpp.createPayment(charge, function (result, err) {
-      this.setState({disableButton: false})
+      that.setState({disableButton: false})
       if (result == "success") {
         // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL。
         browserHistory.push('/mine/wallet')
