780264d40c69fe90b1e1e7fea65b8de0e5542038
wanpeng <wanpeng19860919@163.com>
2017-11-02 18:41:52 +0800

    <Debug>

--- src/util/index.js
+++ src/util/index.js
@@ -54,16 +54,16 @@ export function formatTime(milliseconds, format) {
 export function wechatOauth(nextState, replace) {
   let state = store.getState()
   let isRehydrated = selectIsRehydrated(state)
-  console.log("url:", document.location.href)
-  store.dispatch(fetchWechatJssdkConfig({
-    debug: __DEV__? true: true,
-    jsApiList: ['scanQRCode', 'getLocation'],
-    url: document.location.href,
-    success: (configInfo) => {
-      wx.config(configInfo)
-    },
-    error: (error) => {console.log(error)}
-  }))
+  // console.log("url:", document.location.href)
+  // store.dispatch(fetchWechatJssdkConfig({
+  //   debug: __DEV__? true: true,
+  //   jsApiList: ['scanQRCode', 'getLocation'],
+  //   url: document.location.href,
+  //   success: (configInfo) => {
+  //     wx.config(configInfo)
+  //   },
+  //   error: (error) => {console.log(error)}
+  // }))
   if(!isRehydrated) {
     replace({
       pathname: '/loading',
