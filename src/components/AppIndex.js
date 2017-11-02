e3e65ec6ccc7011dcd6b289a392e28e8871cc9c7
wanpeng <wanpeng19860919@163.com>
2017-11-02 16:25:03 +0800

    <Debug>jssdk调试

--- src/components/AppIndex.js
+++ src/components/AppIndex.js
@@ -4,10 +4,37 @@
 import React, {Component} from 'react'
 import {Link, IndexLink} from 'react-router'
 import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import wx from 'tencent-wx-jssdk'
+import {fetchWechatJssdkConfig} from '../actions/authActions'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-export default class AppIndex extends Component {
+
+
+class AppIndex extends Component {
   constructor(props) {
     super(props)
+  }
+
+  componentWillMount() {
+    const {fetchWechatJssdkConfig} = this.props
+    fetchWechatJssdkConfig({
+      debug: __DEV__? true: false,
+      jsApiList: ['scanQRCode', 'getLocation'],
+      url: window.location.href,
+      success: (configInfo) => {
+        wx.config(configInfo)
+      },
+      error: (error) => {console.log(error)}
+    })
+  }
+
+  onScanQRCode() {
+    wx.scanQRCode({
+      needResult: 0,
+      scanType: ["qrCode","barCode"],
+      success: () => {}
+    })
   }
 
   render() {
@@ -15,9 +42,15 @@ export default class AppIndex extends Component {
       <div style={{height: '100%'}}>
         <IndexLink to="/">主页</IndexLink>
         <WingBlank>
-          <Button type="primary">primary</Button>
+          <Button type="primary" onClick={() => this.onScanQRCode()}>扫一扫</Button>
         </WingBlank>
       </div>
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  fetchWechatJssdkConfig,
+}, dispatch)
+
+export default connect(null, mapDispatchToProps)(AppIndex)
