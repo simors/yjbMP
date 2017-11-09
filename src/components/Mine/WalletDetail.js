39759bf3610eb089d5683c22e63d2f583c00bf37
wanpeng <wanpeng19860919@163.com>
2017-11-09 17:08:08 +0800

    <Debug>

--- src/components/Mine/WalletDetail.js
+++ src/components/Mine/WalletDetail.js
@@ -142,7 +142,7 @@ class WalletDetail extends Component {
                         <text className="content-trip">{formatTime((new Date(item.dealTime)).getTime(),'YYYY-MM-DD HH:mm')}</text>
                       </div>
                       <div className="record-content">
-                        <text className="content-primary">{item.cost + '元'}</text>
+                        <text className="content-primary">{item.amount + '元'}</text>
                         <text className="content-trip">{this.getDealType(item)}</text>
                       </div>
                     </div>
